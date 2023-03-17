f471d5cfa58d1921ff5e681b99b3faf498ec9fa1
Tabitha Lyn <tabithalyn@ymail.com>
2023-03-17 12:55:19 -0400

    Yeah..

---
+++ traveladvise/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import './App.css';
+import Options from './Options';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Options />
+    </>
+  )
+}
+
+export default App;
---
+++ traveladvise/src/Options.jsx
@@ -0,0 +1,8 @@
+export default function Options() {
+  return (<div className="selections">
+      <div className="option"><div className="choice">One</div></div>
+      <div className="option"><div className="choice">Two</div></div>
+      <div className="option"><div className="choice">Three</div></div>
+      <div className="option"><div className="choice">Four</div></div>
+  </div>)
+}
