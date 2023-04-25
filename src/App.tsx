94f84fc78e431715678f62942a01b94815e8aeee
Tabitha Lyn <tabithalyn6@gmail.com>
2023-04-25 11:35:24 -0400

    Yeah.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Container, Col, Row } from 'react-bootstrap';
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>One</Col>
+          <Col>Two</Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default App;
