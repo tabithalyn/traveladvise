472d6c3635db245c7fc48d88fd68ec0200eff4fe
Tabitha Lyn <tabithalyn6@gmail.com>
2023-06-25 20:24:44 -0400

    Few things

--- src/pages/Attractions.tsx
+++ src/pages/Attractions.tsx
@@ -4,12 +4,12 @@ export default function Attractions() {
 
   return (
     <Container fluid>
-    <Row sm={2} md={3} lg={3} xl={5} className='d-flex justify-content-center'>
+    <Row xs={2} sm={2} md={2} lg={3} xl={4} className='d-flex justify-content-center'>
       <Col className='mb-3 d-flex align-items-stretch'>
       <Card>
         <Card.Img variant='top' className='img-fluid' src='src/assets/img/london-eye.png' />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>London Eye</Card.Title>
           <Card.Text>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu sodales sapien, non hendrerit odio. Curabitur a nibh ultrices, maximus purus at, tincidunt massa.
           </Card.Text>
@@ -22,7 +22,7 @@ export default function Attractions() {
       <Card>
         <Card.Img variant='top' className='img-fluid' src='src/assets/img/tower-london.png' />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Tower of London</Card.Title>
           <Card.Text>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu sodales sapien, non hendrerit odio.
           </Card.Text>
